1216c700c57ccf54c3c748bc18a81c56fb503979
Justinyen <email@ornek.com>
2025-12-04 23:58:10 +0300

    feat: Yeni ürün kartı sayfasını Vue.js ile oluşturarak stok, sipariş, BOM ve çerçeve sözleşme bilgilerini ekle.

--- urun_karti.php
+++ urun_karti.php
@@ -586,6 +586,7 @@ if ($urun_kodu == 0) {
     </div>
     </div>
 
+
     <!-- jQuery and Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
